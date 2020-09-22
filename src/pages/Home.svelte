b28e3e6cc9e6b28c01f897520228bb2f13f2433a
Jason Lee <jason@zerodevx.com>
2020-09-22 16:21:08 +0800

    Fix mobile layout

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,7 +3,7 @@ import { fade } from 'svelte/transition'
 </script>
 
 <div class="page" transition:fade>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center text-center">
     <h1 class="text-red-500 text-6xl font-thin my-8">HELLO WORLD!</h1>
     <p class="mx-4 mb-8">An opinionated Svelte project starter template for quick boilerplating.</p>
     <a class="underline text-blue-500 hover:text-blue-700" href="https://github.com/zerodevx/svelte-starter">https://github.com/zerodevx/svelte-starter</a>
