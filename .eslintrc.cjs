5169f83f1a5d2bf0191937922d41fc7d59438408
Jason Lee <jason@zerodevx.com>
2021-09-16 02:30:25 +0800

    Setup baseline

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    { files: ['*.svelte'], plugins: ['svelte3'], processor: 'svelte3/svelte3' },
+    { files: ['*.html'], plugins: ['html'] }
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  }
+}
---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter()
+  },
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ],
+  router: true,
+  ssr: false,
+  trailingSlash: 'never'
+}
+
+export default config
