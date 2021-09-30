ef2972fcdf49aa0f0be9698fa6836220cad3a841
Jason Lee <jason@zerodevx.com>
2021-09-30 17:02:39 +0800

    Stop `prettier` from doing stupid things when no-semis

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
   extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    'no-tabs': 'error',
+    'no-unexpected-multiline': 'error'
+  },
   overrides: [
     { files: ['*.svelte'], plugins: ['svelte3'], processor: 'svelte3/svelte3' },
     { files: ['*.html'], plugins: ['html'] }
