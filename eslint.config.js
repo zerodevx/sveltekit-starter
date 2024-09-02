35dcfe80f4688bb4feca86e319030d78fce95fed
Jason Lee <jason@zerodevx.com>
2024-09-02 21:44:25 +0800

    Setup `eslint@9`

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      'no-tabs': 'error',
+      'no-unexpected-multiline': 'error'
+    }
+  },
+  {
+    ignores: ['temp/']
+  }
+]
