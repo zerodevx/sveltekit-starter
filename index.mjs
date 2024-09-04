00a8379a1b7d6aa9d22630a002cbe3e5d64e6af2
Jason Lee <jason@zerodevx.com>
2024-09-04 23:38:42 +0800

    Flatten the code

--- index.mjs
+++ index.mjs
@@ -7,9 +7,7 @@ if (!name) {
   process.exit(1)
 }
 
-function p(...args) {
-  return path.join(name, ...args)
-}
+const p = (...args) => path.join(name, ...args)
 
 async function patchFiles(files, ...replacers) {
   for (const file of [files].flat()) {
@@ -32,26 +30,29 @@ async function patchPackage(...dependencies) {
   await fs.writeJson(file, pkg, { spaces: 2 })
 }
 
-await spinner('replace favicon', async () => {
-  const req = await fetch('https://cdn.jsdelivr.net/gh/zerodevx/sveltekit-starter/favicon.png')
-  const blob = await req.blob()
-  const buf = await blob.arrayBuffer()
-  await fs.writeFile(p('static', 'favicon.png'), Buffer.from(buf))
-})
+let done
+const loading = spinner('patching...', () => new Promise((resolve) => (done = resolve)))
 
-await spinner('add tailwindcss, iconify and fontsource', async () => {
-  await patchPackage(
-    '+tailwindcss',
-    '+autoprefixer',
-    '+@tailwindcss/typography',
-    '+@fontsource-variable/inter',
-    '+@iconify/tailwind',
-    '+@iconify-json/mdi',
-    '+prettier-plugin-tailwindcss'
-  )
-  await fs.writeFile(
-    p('tailwind.config.js'),
-    `import { addIconSelectors } from '@iconify/tailwind'
+const favicon = await fetch('https://cdn.jsdelivr.net/gh/zerodevx/sveltekit-starter/favicon.png')
+  .then((r) => r.blob())
+  .then((b) => b.arrayBuffer())
+await fs.writeFile(p('static', 'favicon.png'), Buffer.from(favicon))
+
+await patchPackage(
+  '+tailwindcss',
+  '+autoprefixer',
+  '+@tailwindcss/typography',
+  '+@fontsource-variable/inter',
+  '+@iconify/tailwind',
+  '+@iconify-json/mdi',
+  '+prettier-plugin-tailwindcss',
+  '-@sveltejs/adapter-auto',
+  '+@sveltejs/adapter-static'
+)
+
+await fs.writeFile(
+  p('tailwind.config.js'),
+  `import { addIconSelectors } from '@iconify/tailwind'
 import typography from '@tailwindcss/typography'
 import dt from 'tailwindcss/defaultTheme'
 
@@ -67,83 +68,76 @@ export default {
   },
   plugins: [addIconSelectors(['mdi']), typography]
 }`
-  )
-  await fs.writeFile(
-    p('postcss.config.js'),
-    `/** @type {import('postcss-load-config').Config} */
+)
+
+await fs.writeFile(
+  p('postcss.config.js'),
+  `/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {}
   }
 }`
-  )
-  await fs.writeFile(
-    p('src', 'app.pcss'),
-    `/* Write your global styles here, in PostCSS syntax */
+)
+
+await fs.writeFile(
+  p('src', 'app.css'),
+  `/* Write your global styles here, in PostCSS syntax */
 @tailwind base;
 @tailwind components;
 @tailwind utilities;`
-  )
-  await fs.writeFile(
-    p('src', 'routes', '+layout.svelte'),
-    `<script>
+)
+
+await fs.writeFile(
+  p('src', 'routes', '+layout.svelte'),
+  `<script>
 import '@fontsource-variable/inter'
-import '../app.pcss'
+import '../app.css'
 </script>
 
 <slot />`
-  )
-  await patchFiles(p('src', 'routes', '+page.svelte'), [
-    `</h1>`,
-    `</h1>\n<span class="iconify mdi--heart text-xl text-red-600 animate-pulse" />\n`
-  ])
-  await patchFiles(
-    p('svelte.config.js'),
-    [`import`, `import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';\nimport`],
-    [`\n};`, `, preprocess: [vitePreprocess()]\n};`]
-  )
+)
+
+await fs.writeFile(p('src', 'routes', '+layout.js'), `export const prerender = true\n`)
+
+await patchFiles(p('src', 'routes', '+page.svelte'), [
+  `</h1>`,
+  `</h1>\n<span class="iconify mdi--heart text-xl text-red-600 animate-pulse" />\n`
+])
+
+await patchFiles(
+  p('svelte.config.js'),
+  [`import`, `import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';\nimport`],
+  [
+    `adapter-auto';`,
+    `adapter-static';\nimport { readFileSync } from 'node:fs';\n\nconst { version: name } = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'));`
+  ],
+  [`adapter()`, `adapter(), version:{name}`],
+  [`\n};`, `, preprocess: [vitePreprocess()]\n};`]
+)
+
+await patchFiles(p('eslint.config.js'), [
+  `languageOptions`,
+  `rules:{'no-tabs':'error','no-unexpected-multiline':'error'}, languageOptions`
+])
+
+const prettier = await fs.readJson(p('.prettierrc'))
+prettier.plugins = [...prettier.plugins, 'prettier-plugin-tailwindcss']
+await fs.writeJson(p('.prettierrc'), {
+  ...prettier,
+  printWidth: 100,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  proseWrap: 'always',
+  svelteSortOrder: 'options-scripts-markup-styles',
+  svelteIndentScriptAndStyle: false
 })
 
-await spinner('patch prettier', async () => {
-  let config = await fs.readJson(p('.prettierrc'))
-  config.plugins = [...config.plugins, 'prettier-plugin-tailwindcss']
-  await fs.writeJson(p('.prettierrc'), {
-    ...config,
-    printWidth: 100,
-    useTabs: false,
-    semi: false,
-    singleQuote: true,
-    trailingComma: 'none',
-    proseWrap: 'always',
-    svelteSortOrder: 'options-scripts-markup-styles',
-    svelteIndentScriptAndStyle: false
-  })
-})
-
-await spinner('patch eslint', async () => {
-  await patchFiles(p('eslint.config.js'), [
-    `languageOptions`,
-    `rules:{'no-tabs':'error','no-unexpected-multiline':'error'}, languageOptions`
-  ])
-})
-
-await spinner('add adapter-static', async () => {
-  await patchFiles(p('svelte.config.js'), [`adapter-auto`, `adapter-static`])
-  await patchPackage(name, '-@sveltejs/adapter-auto', '+@sveltejs/adapter-static')
-  await fs.writeFile(p('src', 'routes', '+layout.js'), `export const prerender = true\n`)
-})
-
-await spinner('add versioning', async () => {
-  await patchFiles(
-    p('svelte.config.js'),
-    [
-      `static';`,
-      `static';\nimport { readFileSync } from 'node:fs'\n\nconst { version: name } = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'))\n`
-    ],
-    [`adapter()`, `adapter(), version:{name}`]
-  )
-})
+done()
+await loading
 
 echo`
 All done! Complete the setup with:
